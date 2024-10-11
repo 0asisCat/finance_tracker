846604a996f6ae174aa6b1a973eb46bb889225b4
B!anca <155007644+0asisCat@users.noreply.github.com>
2024-10-11 18:34:49 +0800

    Put.script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const mainForm = document.querySelector("#form");
+const weekButton = document.querySelector("#week-button")
 
+weekButton.onclick = addWeek();
+
+function addWeek(){
+    const weekNo = 0
+    const HTMLString = `
+    <fieldset>
+        <legend>Week ${weekNo}</legend>
+    </fieldset>
+    `;
+}
