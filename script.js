6c1e42d0eb80f294f7075e7a3bee8e129ff0c356
B!anca <155007644+0asisCat@users.noreply.github.com>
2024-10-14 22:18:03 +0800

    put.script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const mainForm = document.querySelector("#form");
-const weekBtn = document.querySelector("#week-btn")
 const monthOption = document.querySelector("#entry-dropdown");
+const tableContainer = document.querySelector("#table-container");
+const tableLabels = document.querySelector("#table-labels");
+
+const weekBtn = document.querySelector("#week-btn")
+const columnBtn = document.querySelector("#add-column");
 let weekNo = 0;
 let isFour = false;
 
@@ -10,26 +14,36 @@ function addWeek(){
         weekBtn.style.display('none');
     }
     const HTMLString = `
-    <fieldset>
-        <legend>Week ${weekNo}</legend>
+    <fieldset id="table-box">
+        <legend class="strong">Week ${weekNo}</legend>
         <table>
-            <tr>
+            <tr id="table-labels">
                 <th>Item</th>
                 <th>Number</th>
                 <th>Cost</th>
             </tr>
-            <tr>
-                <td> </td>
-                <td> </td>
-                <td> </td>
-            </tr>
         </table>
+        <button id="add-column" type="button">Add</button>
     </fieldset>
     `;
-    mainForm.insertAdjacentHTML('beforeend', HTMLString);
+    tableContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+function addColumn(){
+    const HTMLString = `
+    <table>
+        <tr>
+            <td><input type="text"></th>
+            <td><input type="text"></th>
+            <td><input type="text"></th>
+        </tr>
+    </table>
+    `
+    tableLabels.insertAdjacentHTML('beforeend', HTMLString);
 }
 
 weekBtn.addEventListener("click", addWeek);
+columnBtn.addEventListener("click", addColumn);
 
 // create a table: for item, number, cost
 // within fieldset create button to add rows and to add total cost, w/out losing the input
